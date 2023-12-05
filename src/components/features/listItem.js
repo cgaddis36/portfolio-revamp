f4faa7e8bc7699af05adcede443de28cc3e092cf
Chase Gaddis <cgaddis36@gmail.com>
2023-12-05 15:00:46 -0500

    add logic for showing details

--- src/components/features/listItem.js
+++ src/components/features/listItem.js
@@ -1,10 +1,35 @@
 import styles from '../styles/listItem.module.css';
 
-function ListItem({title}) {
-
+function ListItem({
+    title, 
+    details,
+    active,
+    setActive,
+    items
+}) {
+  
+    const activeIndexer = items.findIndex((t) => t === title) + 1
+    const setActiveItem = (title) => {
+        if(active === activeIndexer) {
+            return 0
+        } else {
+            return activeIndexer
+        }
+    }
     return(
         <div className={styles.item}>
-            {title}
+            <div 
+                className={styles.title}
+                onClick={() => setActive(setActiveItem(activeIndexer))}
+                >
+                {title}
+            </div>
+            {
+                active === activeIndexer ?
+                details
+                :
+                null
+            }
         </div>
     )
 }
