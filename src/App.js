d9638315ea724bd3a235a0433f15fde5ba622f31
Chase Gaddis <cgaddis36@gmail.com>
2023-12-05 14:58:32 -0500

    add logic for ListItems

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
 import ListItem from './components/features/listItem';
+import { useState } from 'react';
+import EducationDetails from './components/features/educationDetails';
+import ExperienceDetails from './components/features/experienceDetails'
+import ProjectDetails from './components/features/projectDetails';
 
 function App() {
+  const items = ["Projects", "Experience", "Education"]
+  const [active, setActive] = useState(0)
   return (
     <div className="App">
-      <ListItem title={"Projects"}/>
-      <ListItem title={"Experience"}/>
-      <ListItem title={"Education"}/>
+      <ListItem 
+        title={items[0]}
+        details={<ProjectDetails/>}
+        active={active}
+        setActive={setActive}
+        items={items}
+        />
+      <ListItem 
+        title={items[1]}
+        details={<ExperienceDetails/>}
+        active={active}
+        setActive={setActive}
+        items={items}
+        />
+      <ListItem 
+        title={items[2]}
+        details={<EducationDetails/>}
+        active={active}
+        setActive={setActive}
+        items={items}
+        />
     </div>
   );
 }
