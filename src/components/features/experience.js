f5cd075ef8f95671840f92bae7c9e0107eecfac5
Chase Gaddis <cgaddis36@gmail.com>
2024-01-07 12:11:05 -0500

    add base experience

--- src/components/features/experience.js
+++ src/components/features/experience.js
@@ -1,6 +1,25 @@
 import styles from '../styles/experience.module.css'
 
 function Experience() {
+    const userExperience = [
+        {
+            name: 'Game Drive',
+            teamSize: 8,
+            techStack: 'Go.js, React, postgres, Notion',
+            hostedUrl: 'https://shrimp-republic.com',
+            githubFe: '',
+            githubBe: '',
+        },
+        {
+            name: 'ChessMeito',
+            teamSize: 2,
+            techStack: 'React Native, React, MongoDB, Python',
+            hostedUrl: 'https://shrimp-republic.com',
+            githubFe: 'private',
+            githubBe: 'private',
+        }
+
+    ]
     return(
         <div className={styles.details}>
             Details experience
