f2e3a116e53195b642cc57ac792d6d925b6a8b64
Chase Gaddis <cgaddis36@gmail.com>
2024-01-08 18:07:12 -0500

    add base edu

--- src/components/features/education.js
+++ src/components/features/education.js
@@ -1,12 +1,37 @@
-
 import styles from '../styles/education.module.css'
 
 function Education() {
-    return(
-        <div className={styles.details}>
-            Details Baby
-        </div>
+  const userEducation = [
+    {
+      name: 'Turing School of Software & Design',
+      location: 'Denver, CO',
+      degree: 'Technical Certificate',
+      major: 'Back-End Engineering',
+      graduationDate: 'November 2020',
+    },
+    {
+      name: 'University of Georgia',
+      location: 'Athens, GA',
+      degree: 'Consumer Economic, Emphasis: Applied Analytics & Research',
+      major: 'Back-End Engineering',
+      graduationDate: 'December 2017',
+    }
+  ]
+
+  const renderEdu = (education) => {
+    return (
+      education.map((edu) => 
+      <div>
+      {edu.name}
+      </div>
+      )
     )
+  }
+  return(
+    <div className={styles.details}>
+        {renderEdu(userEducation)}
+    </div>
+  )
 }
 
 export default Education
