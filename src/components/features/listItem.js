e2150716896e834be3cbc6dfe6e9b7e9ca9ee631
Chase Gaddis <cgaddis36@gmail.com>
2024-01-08 18:08:11 -0500

    active conditionals

--- src/components/features/listItem.js
+++ src/components/features/listItem.js
@@ -1,4 +1,5 @@
 import styles from '../styles/listItem.module.css';
+import chevronRight from '../images/chevronRight.svg'
 
 function ListItem({
     title, 
@@ -8,29 +9,51 @@ function ListItem({
     items
 }) {
   
-    const activeIndexer = items.findIndex((t) => t === title) + 1
-    const setActiveItem = (title) => {
-        if(active === activeIndexer) {
-            return 0
-        } else {
-            return activeIndexer
-        }
+  const activeIndexer = items.findIndex((t) => t === title) + 1
+  const setActiveItem = (title) => {
+    if(active === title) {
+      return 0
+    } else {
+      return activeIndexer
     }
-    return(
+  }
+  return(
+    <div className={styles.main}>
+      {
+        active !== 0 ? 
         <div className={styles.item}>
-            <div 
-                className={styles.title}
-                onClick={() => setActive(setActiveItem(activeIndexer))}
-                >
-                {title}
-            </div>
-            {
-                active === activeIndexer ?
-                details
-                :
-                null
-            }
+          <div 
+            className={active === activeIndexer ? styles.activeTitle : styles.invisibleTitle}
+            onClick={() => setActive(setActiveItem(activeIndexer))}
+            >
+            {title}
+            <img src={chevronRight} className={active === activeIndexer ? styles.activeArrow : styles.inActiveArrow}/>
+          </div>
+          
+          {
+            active === activeIndexer ?
+            details
+            :
+            null
+          }
         </div>
-    )
+        :
+        <div className={styles.item}>
+          <div 
+            className={styles.inactiveTitle}
+            onClick={() => setActive(setActiveItem(activeIndexer))}
+            >
+            {title}
+          </div>
+          {
+            active === activeIndexer ?
+            details
+            :
+            null
+          }
+        </div>
+      }
+    </div>
+  )
 }
 export default ListItem
