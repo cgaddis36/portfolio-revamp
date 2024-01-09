857c1efc381206b73675c11b69e7636683d6ef5d
Chase Gaddis <cgaddis36@gmail.com>
2024-01-08 22:15:05 -0500

    update experience block

--- src/components/features/experience.js
+++ src/components/features/experience.js
@@ -3,20 +3,20 @@ import styles from '../styles/experience.module.css'
 function Experience() {
     const userExperience = [
       {
+        name: 'ChessMeito',
+        teamSize: 2,
+        startDate: '9/2023',
+        endDate: '1/2024',
+        position: 'Full-Stack Mobile Engineer',
+        description: 'Remote back-end engineering apprenticeship paired with a Front-end engineer to deploy a micro-service on Heroku designed to help Veterans locate the most affordable physicians in their area.',
+      },
+      {
         name: 'Game Drive',
         teamSize: 1,
         startDate: '7/2021',
         endDate: '7/2022',
         position: 'Full-Stack Engineer',
         description: 'Developed, designing and implementing web applications linked to Salesforce to optimize processes within their internal operations.',
-      },
-      {
-        name: 'ChessMeito',
-        teamSize: 3,
-        startDate: '9/2023',
-        endDate: '1/2023',
-        position: 'Full-Stack Mobile Engineer',
-        description: 'Remote back-end engineering apprenticeship paired with a Front-end engineer to deploy a micro-service on Heroku designed to help Veterans locate the most affordable physicians in their area.',
       },
       {
         name: 'Leap',
@@ -38,16 +38,33 @@ function Experience() {
 
     const renderExperience = (experience) => {
       return experience.map((exp) => 
-      <div>
-        {
-          exp.name
-        }
+      <div className={styles.experience}>
+        <div className={styles.namePositionRow}>
+          <div className={styles.name}>
+            {exp.name} 
+          </div>
+          <div className={styles.position}>
+            {exp.position} 
+          </div>
+
+        </div>
+        <div className={styles.description}>
+          {exp.description} 
+        </div>
+        <div className={styles.dateTeamRow}>
+          <div className={styles.teamSize}>
+            Team Size: {exp.teamSize} 
+          </div>
+          <div className={styles.date}>
+            {`${exp.startDate} - ${exp.endDate}`} 
+          </div>
+        </div>
       </div>
       )
     }
 
   return(
-    <div className={styles.details}>
+    <div className={styles.main}>
       {renderExperience(userExperience)}
     </div>
   )
